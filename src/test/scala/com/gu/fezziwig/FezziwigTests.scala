13175c4cac772a848a2f1ed3feff1a3b80ec8e7c
Emily Bourke <emily.bourke@guardian.co.uk>
2024-02-13 14:42:00 +0000

    Re-enable other tests

--- src/test/scala/com/gu/fezziwig/FezziwigTests.scala
+++ src/test/scala/com/gu/fezziwig/FezziwigTests.scala
@@ -87,78 +87,78 @@ class FezziwigTests extends AnyFlatSpec with Matchers  {
     diffJ should be(JsonPatch(Nil))
   }
 
-  // it should "round-trip scrooge thrift models" in {
-  //   val jsonString =
-  //     """
-  //       |{
-  //       |  "b": {
-  //       |    "u": {
-  //       |      "c": {
-  //       |        "s": "test"
-  //       |      }
-  //       |    }
-  //       |  },
-  //       |  "foo": "test-foo",
-  //       |  "bar": 3,
-  //       |  "e": "ENUM_A",
-  //       |  "intMap": {
-  //       |    "k": [2]
-  //       |  },
-  //       |  "x": {
-  //       |    "s": "x"
-  //       |  }
-  //       |}
-  //     """.stripMargin
+  it should "round-trip scrooge thrift models" in {
+    val jsonString =
+      """
+        |{
+        |  "b": {
+        |    "u": {
+        |      "c": {
+        |        "s": "test"
+        |      }
+        |    }
+        |  },
+        |  "foo": "test-foo",
+        |  "bar": 3,
+        |  "e": "ENUM_A",
+        |  "intMap": {
+        |    "k": [2]
+        |  },
+        |  "x": {
+        |    "s": "x"
+        |  }
+        |}
+      """.stripMargin
 
-  //   val jsonBefore: Json = parse(jsonString).toOption.get
+    val jsonBefore: Json = parse(jsonString).toOption.get
 
-  //   val decoded: StructA = jsonBefore.as[StructA].toOption.get
+    val decoded: StructA = jsonBefore.as[StructA].toOption.get
 
-  //   val jsonAfter: Json = decoded.asJson
+    val jsonAfter: Json = decoded.asJson
 
-  //   val diffJ = diff[Json, JsonPatch[Json]](jsonBefore, jsonAfter)
-  //   if (diffJ != JsonPatch(Nil)) println(s"${diffJ.toString}")
-  //   diffJ should be(JsonPatch(Nil))
-  // }
+    val diffJ = diff[Json, JsonPatch[Json]](jsonBefore, jsonAfter)
+    if (diffJ != JsonPatch(Nil)) println(s"${diffJ.toString}")
+    diffJ should be(JsonPatch(Nil))
+  }
 
-  // it should "accumulate errors" in {
-  //   //In the following json, the fields 's', 'foo' and 'x' have incorrect types
-  //   val expectedFailures = List(
-  //     DecodingFailure("String", List(DownField("s"), DownField("c"), DownField("u"), DownField("b"))),
-  //     DecodingFailure("String", List(DownField("foo"))),
-  //     DecodingFailure("Expected an object", List(DownField("x")))
-  //   )
+  it should "accumulate errors" in {
+    //In the following json, the fields 's', 'foo' and 'x' have incorrect types
+    val expectedFailures = List(
+      DecodingFailure("String", List(DownField("s"), DownField("c"), DownField("u"), DownField("b"))),
+      DecodingFailure("String", List(DownField("foo"))),
+      DecodingFailure("Expected an object", List(DownField("x")))
+    )
 
-  //   val jsonString =
-  //     """
-  //       |{
-  //       |  "b": {
-  //       |    "u": {
-  //       |      "c": {
-  //       |        "s": 1
-  //       |      }
-  //       |    }
-  //       |  },
-  //       |  "foo": 1,
-  //       |  "bar": 3,
-  //       |  "e": "ENUM_A",
-  //       |  "intMap": {
-  //       |    "k": [2]
-  //       |  },
-  //       |  "x": 2
-  //       |}
-  //     """.stripMargin
+    val jsonString =
+      """
+        |{
+        |  "b": {
+        |    "u": {
+        |      "c": {
+        |        "s": 1
+        |      }
+        |    }
+        |  },
+        |  "foo": 1,
+        |  "bar": 3,
+        |  "e": "ENUM_A",
+        |  "intMap": {
+        |    "k": [2]
+        |  },
+        |  "x": 2
+        |}
+      """.stripMargin
 
-  //   val dec = Decoder[StructA]
+    val dec = Decoder[StructA]
 
-  //   val jsonBefore: Json = parse(jsonString).toOption.get
+    val jsonBefore: Json = parse(jsonString).toOption.get
 
-  //   val result = dec.decodeAccumulating(jsonBefore.hcursor)
+    val result = dec.decodeAccumulating(jsonBefore.hcursor)
 
-  //   result.isInvalid should be(true)
-  //   result.swap.foreach(nel => {
-  //     nel.toList.length should be(3)
-  //     nel.toList should be(expectedFailures)
-  //   })
-  // }
+    result.isInvalid should be(true)
+    result.swap.foreach(nel => {
+      nel.toList.length should be(3)
+      nel.toList should be(expectedFailures)
+    })
+  }
 }
