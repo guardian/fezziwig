af66a38c096fc78d879162bac5f48b5beb563a5f
Emily Bourke <emily.bourke@guardian.co.uk>
2026-08-13 17:08:10 +0100

    DO_NOT_MERGE: Debug updating scala to 2.13.15
    Updating to Scala 2.13.15 causes the compiler to crash when compiling
    these tests. To debug, I’ve tried removing parts of the tests until it
    compiles again: this is the minimal set I can remove. I suspect the
    problem is in the union macros, and the real solution is removing the
    deriveEncoder and deriveDecoder calls for Union1.

--- src/test/scala/com/gu/fezziwig/FezziwigTests.scala
+++ src/test/scala/com/gu/fezziwig/FezziwigTests.scala
@@ -20,20 +20,11 @@ import org.scalatest.matchers.should.Matchers
 import shapeless._
 
 class FezziwigTests extends AnyFlatSpec with Matchers  {
-  implicit val union1Decoder: Decoder[Union1] = deriveDecoder
-  implicit val union1Encoder: Encoder[Union1] = deriveEncoder
-
-  implicit val structBDecoder: Decoder[StructB] = deriveDecoder
-  implicit val structBEncoder: Encoder[StructB] = deriveEncoder
-
   implicit val structCDecoder: Decoder[StructC] = deriveDecoder
   implicit val structCEncoder: Encoder[StructC] = deriveEncoder
 
   implicit val structDDecoder: Decoder[StructD] = deriveDecoder
   implicit val structDEncoder: Encoder[StructD] = deriveEncoder
-
-  implicit val structADecoder: Decoder[StructA] = deriveDecoder
-  implicit val structAEncoder: Encoder[StructA] = deriveEncoder
 
   implicit val recTreeDecoder: Decoder[RecTree] = deriveDecoder
   implicit val recTreeEncoder: Encoder[RecTree] = deriveEncoder
@@ -168,93 +159,10 @@ class FezziwigTests extends AnyFlatSpec with Matchers  {
       """.stripMargin)
   }
 
-  it should "round-trip StructB (struct containing a union)" in {
-    testRoundTrip[StructB](
-      """
-        |{
-        |  "u" : {
-        |    "c" : {
-        |      "s" : "hello"
-        |    }
-        |  }
-        |}
-      """.stripMargin)
-  }
-
-  it should "round-trip StructA (a more nested struct)" in {
-    testRoundTrip[StructA](
-      """
-        |{
-        |  "b": {
-        |    "u": {
-        |      "c": {
-        |        "s": "test"
-        |      }
-        |    }
-        |  },
-        |  "foo": "test-foo",
-        |  "bar": 3,
-        |  "e": "ENUM_A",
-        |  "intMap": {
-        |    "k": [2]
-        |  },
-        |  "x": {
-        |    "s": "x"
-        |  }
-        |}
-      """.stripMargin)
-  }
-
   it should "follow circe default behavior of serialising optional fields as nulls rather than missing" in {
     implicit val structWithOptionalEncoder: Encoder[StructWithOptional] = deriveEncoder
     val jsonAfter: Json = StructWithOptional().asJson
     jsonAfter.asObject.flatMap(_("optionalField")).value.isNull should be(true)
-  }
-
-  it should "accumulate errors" in {
-    //In the following json, the fields 's', 'foo' and 'x' have incorrect types
-    val expectedFailures = List(
-      DecodingFailure("Got value '1' with wrong type, expecting string", List(DownField("s"), DownField("c"), DownField("u"), DownField("b"))),
-      DecodingFailure("Got value '1' with wrong type, expecting string", List(DownField("foo"))),
-      DecodingFailure("Missing required field", List(DownField("s"), DownField("x")))
-    )
-
-    val jsonString =
-      """
-        |{
-        |  "b": {
-        |    "u": {
-        |      "c": {
-        |        "s": 1
-        |      }
-        |    }
-        |  },
-        |  "foo": 1,
-        |  "bar": 3,
-        |  "e": "ENUM_A",
-        |  "intMap": {
-        |    "k": [2]
-        |  },
-        |  "x": 2
-        |}
-      """.stripMargin
-
-    val dec = Decoder[StructA]
-
-    val jsonBefore: Json = parse(jsonString).toOption.get
-
-    val result = dec.decodeAccumulating(jsonBefore.hcursor)
-
-    result.isInvalid should be(true)
-    result.swap.foreach(nel => {
-      nel.toList.length should be(3)
-      nel.toList.zip(expectedFailures).foreach {
-        case (found, expected) => {
-          found.message shouldBe expected.message
-          found.history shouldBe expected.history
-        }
-      }
-    })
   }
 
   it should "decode a missing optional field without default" in {
@@ -397,40 +305,5 @@ class FezziwigTests extends AnyFlatSpec with Matchers  {
     val decoded: Decoder.Result[DefaultTestStruct] = jsonBefore.as[DefaultTestStruct]
 
     decoded shouldBe Right(DefaultTestStruct(Some(1), 2, 3, 4, 5, 6, List()))
-  }
-
-  it should "encode an UnknownUnionField successfully as an object (dropping field content)" in {
-    val tfieldBlob = TFieldBlob(new TField("e", TType.STRUCT, 3), Buf.slowFromHexString("ff1e"))
-    val x: Union1 = Union1.UnknownUnionField(tfieldBlob)
-
-    val expectedString =
-      """
-        | {
-        |   "__unknownUnionField": {
-        |      "name": "e",
-        |      "type": 12,
-        |      "id": 3
-        |   }
-        | }
-      """.stripMargin
-    val expectedJson = parse(expectedString)
-
-    Right(x.asJson) should be (expectedJson)
-
-    val decoded: Either[DecodingFailure, Union1] = expectedJson.toOption.get.as[Union1]
-    decoded should be (Right(Union1.UnknownUnionField(tfieldBlob.copy(content = Buf.Empty))))
-  }
-
-  it should "round-trip an UnknownUnionField successfully" in {
-    testRoundTrip[Union1](
-      """
-        | {
-        |   "__unknownUnionField": {
-        |      "name": "e",
-        |      "type": 12,
-        |      "id": 3
-        |   }
-        | }
-      """.stripMargin)
   }
 }
